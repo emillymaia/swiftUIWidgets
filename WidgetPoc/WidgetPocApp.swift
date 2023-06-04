7aaa7e109bcd84be2b817033175f3ab2f3e9b013
Emilly Maia <emillypereira1290@gmail.com>
2023-06-04 16:19:06 -0300

    Initial Commit

---
+++ WidgetPoc/WidgetPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPocApp.swift
+//  WidgetPoc
+//
+//  Created by Emilly Maia on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
